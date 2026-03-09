216645f164a97a2b8f1a81e07f40cb10b3dc565f
CronnoTimer <angelitachi25@gmail.com>
2026-03-09 12:26:36 -0600

    creación y adaptación de archivos HTML y PHP para el registro y login de usuarios, incluyendo la creación de nuevas versiones en P HP de los archivos Miembros y Registro, así como la adición de un nuevo archivo de login en HTML. La finalidad de esto es poder mostrar una demo de la funcionalidad de registro y login de usuarios en un sitio web, usando solo html para su correcta visualización en github pages.

--- Registro.php
+++ Registro.php
@@ -1,5 +1,7 @@
 <?php
 // registro.php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 // Preparar variables para el template
 $error = isset($_GET['error']) ? htmlspecialchars($_GET['error']) : null;
